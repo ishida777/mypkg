c2b9d954d02d3a4bd28eb4751f76af514813b49a
Kohta Yoshiba <f.leaves.y.28@gmail.com>
2023-12-31 17:18:01 +0900

    re

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -4,11 +4,9 @@ from std_msgs.msg import Int16
 
 class Talker():
     def __init__(self,node):
-        rclpy.init()
-        self.node = Node("talker")
-        self.pub = node.node_publisher(Int16, "countup", 10)
+        self.pub = node.create_publisher(Int16, "countup", 10)
         self.n = 0
-        node.node.create_timer(0.5, self.cb)
+        node.create_timer(0.5, self.cb)
 
     def cb (self):
         msg = Int16()
@@ -16,8 +14,11 @@ class Talker():
         self.pub.publish(msg)
         self.n += 1
 
-if __name__ =='__main__':
-    talker = Talker()
-    rclpy.spin(talker.node)
-    talker.node.destroy_node()
-    rclpy.shutdown()
+def main():
+    rclpy.init()
+    node = Node("talker")
+    talker = Talker(node)
+    rclpy.spin
+
+if __name__ == '__main__':
+    main()
