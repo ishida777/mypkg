3e57c89e5008e6efe1755ca65cdbedce3024aec1
Kohta Yoshiba <f.leaves.y.28@gmail.com>
2024-01-04 01:02:52 +0900

    test.bash

--- test/test.bash
+++ test/test.bash
@@ -6,6 +6,12 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
+
+source /opt/ros/foxy/setup.bash
+source install/local_setup.bash
+
+python -c "import rclpy; rclpy.init()"
+
 timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
